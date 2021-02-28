5ed1993b036dd91bbe798971aa79097cbbc5eb2e
Alisheva Azalia <azalia.alischeva@yandex.ru>
2021-02-28 19:31:27 +0300

    manage.py runs everything one after another, prints price changes and name

--- mysite/manage.py
+++ mysite/manage.py
@@ -2,7 +2,36 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import websocket
+import json
 
+def on_message(ws, message):
+    mes_json = json.loads(message)
+    print(mes_json['data'][0]['p'], mes_json['data'][0]['s'])
+
+def on_error(ws, error):
+    print(error)
+
+
+def on_close(ws):
+    print("### closed ###")
+
+
+def on_open(ws):
+    ws.send('{"type":"subscribe","symbol":"AAPL"}')
+    ws.send('{"type":"subscribe","symbol":"AMZN"}')
+    ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
+    ws.send('{"type":"subscribe","symbol":"IC MARKETS:1"}')
+
+
+def websckt():
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("wss://ws.finnhub.io?token=c0t96iv48v6r4maen2g0",
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.on_open = on_open
+    ws.run_forever()
 
 def main():
     """Run administrative tasks."""
@@ -16,6 +45,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+    websckt()
 
 
 if __name__ == '__main__':
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,7 +3,14 @@ import requests
 
 
 def index(request):
-    url = "https://finnhub.io/api/v1/quote?symbol=AAPL&token=sandbox_c0t96iv48v6r4maen2gg"
-    r = requests.get(url)
-    print(r.text)
+    """url = "https://finnhub.io/api/v1/quote?symbol={}&token=sandbox_c0t96iv48v6r4maen2gg"
+    symbol = 'AMZN'
+    r = requests.get(url.format(symbol)).json()
+    quote = {
+        'symbol': symbol,
+        'price': r['c'],
+        'chng': (r['c']-r['pc']),
+        'chng%': (r['c']-r['pc'])*100/r['pc'],
+    }
+    print(quote, r['o'])"""
     return render(request, 'layout.html')
